359016e1a560eb089f629c7aa7817c11b5d4ff4d
opilane <opilane@HP-CZC6297B8P>
2022-03-07 11:01:05 +0200

    Add project files.

---
+++ Inimene.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_c_sharp
+{
+    class Inimene
+    {
+        string eesnimi;//поля
+        int vanus;
+        string staatus;
+        string perekonanimi;
+        Emakeel emakeel;
+        public Inimene() { }//конструктор
+
+        public Inimene(string Eesnimi, int Vanus, Emakeel emakeel)
+        {
+            eesnimi = Eesnimi;
+            this.emakeel = emakeel;
+            vanus = Vanus;
+        }
+
+        public Inimene(int Vanus)
+        {
+            vanus = Vanus;
+        }
+
+        public Inimene(string eesnimi, string perekonanimi)
+        {
+            this.eesnimi = eesnimi;
+            this.perekonanimi = perekonanimi;
+        }
+
+        public Emakeel Emakeel
+        {
+            get { return emakeel; }
+        }
+
+        public string Eesnimi
+        {
+            set
+            {
+                if (eesnimi == null) eesnimi = value;
+            }
+            get { return eesnimi; }
+
+
+        }//св-ва
+
+
+        public int Vanus
+        {
+            set
+            { vanus = value;
+            }
+            get { return vanus; }
+
+
+        }
+        public string Staatus
+        {
+
+            get
+            {
+                var staatus = "";
+                if (vanus < 7)
+                { staatus = "vaike laps"; }
+                else if (vanus < 17)
+                { staatus = "kooli laps"; }
+                else if (vanus < 65)
+                { staatus = "tööline laps"; }
+                else
+                { staatus = "senior"; }
+                return staatus; }
+
+        }
+
+
+        public void Tervetame()
+        {
+            Console.WriteLine("Tere");
+            Console.WriteLine("Minu nimi on {0} . Olen {1} aastat vana. Minu staatus {2} ", eesnimi, vanus, staatus);
+        }//метод
+        public string Perekonanimi
+        {
+            set
+            {
+                if (perekonanimi == null) perekonanimi = value;
+            }
+            get { return perekonanimi; }
+
+
+        }
+
+        }
+    }
---
+++ Opilane.cs
@@ -0,0 +1,71 @@
+﻿using Class_c_sharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CCCCC2
+{
+    class Opilane : Inimene
+    {
+        string ruhm;
+        double hinne;
+        bool toetus;
+
+        public Opilane() { }
+
+        public Opilane(string Eesnimi, string Perekonanimi, string Ruhm) : base(Eesnimi, Perekonanimi)
+        {
+            ruhm = Ruhm;
+        }
+        public string Ruhm 
+        {
+            set { ruhm = value; }
+            get { return ruhm; }
+        }
+
+        public double Hinne
+        {
+            set
+            {  if (value > 1 && value < 5) hinne = value;}
+            get { return hinne; }
+        }
+
+        public bool Toestus
+        {
+            get
+            {
+                if (hinne > 3)
+                {
+                    toetus = true;
+                }
+                else
+                {
+                    toetus = false;
+                }
+                return toetus;
+            }
+        }
+
+        public void Info()
+        {
+            string t = "";
+            Console.WriteLine($"Minu nimi on {Eesnimi},{Perekonanimi} ");
+            if (Toestus == true)
+            {
+                t = "Saan toetust";
+            }
+            else if (Toestus == false)
+            {
+                t = "Saan toetust";
+            }
+            else
+            {
+                t = "Saan toetust";
+            }
+        }
+        
+    }
+
+    
+}
